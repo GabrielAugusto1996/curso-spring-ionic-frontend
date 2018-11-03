620831142a8ea30a9b9d78d97bb03454e03005e5
GabrielAugusto <biga00145@gmail.com>
2018-11-03 17:10:42 -0300

    Listando categorias

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,5 +1,7 @@
+import { CategoriaDTO } from './../../models/categoria.model';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { CategoriaService } from '../../services/domain/categoria.service';
 
 @IonicPage()
 @Component({
@@ -8,7 +10,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CategoriasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  listCategoria: CategoriaDTO[] = [];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private categoriaService: CategoriaService) {
+  }
+
+  ionViewDidLoad() {
+    this.categoriaService.findAll().subscribe(res => {
+      this.listCategoria = res;
+      console.log(this.listCategoria);
+    }, err => {
+      console.log(err);
+    });
   }
 
 }
