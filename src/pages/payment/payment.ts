ffb8189c5877a430b45e19193607ca302e61b3bb
GabrielAugusto <biga00145@gmail.com>
2018-11-10 18:43:04 -0200

    Tela de confirmação de pedido

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -30,6 +30,7 @@ export class PaymentPage {
 
   proximaPagina() {
     this.pedido.pagamento = this.formGroup.value;
+    this.navCtrl.setRoot('OrderConfirmationPage', {pedido: this.pedido});
   }
 
 }
